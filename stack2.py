ee6e502b12986a29567d8828c500db91d5c23a35
Srujana-Math <167081923+Srujana-Math@users.noreply.github.com>
2024-04-22 11:09:54 +0530

    array implementation of stack

---
+++ stack2.py
@@ -0,0 +1,48 @@
+def push(stack, val):
+    stack.insert(0, val)
+    print(val, "inserted")
+ 
+def pop(stack):
+    if len(stack) == 0:
+        print("Stack is empty, can't delete anything")
+        return 
+ 
+    print(stack[0], "deleted")
+    stack.pop(0)
+ 
+def printStack(stack):
+    if len(stack) == 0:
+        print("Stack is empty, nothing to print")
+        return 
+    print(stack)
+ 
+ 
+stack = []
+push(stack, 10)
+push(stack, 20)
+push(stack, 30)
+push(stack, 40)
+push(stack, 50)
+push(stack, 60)
+printStack(stack)
+ 
+pop(stack)
+printStack(stack)
+ 
+pop(stack)
+printStack(stack)
+ 
+pop(stack)
+printStack(stack)
+ 
+pop(stack)
+printStack(stack)
+ 
+pop(stack)
+printStack(stack)
+ 
+pop(stack)
+printStack(stack)
+ 
+pop(stack)
+printStack(stack)
