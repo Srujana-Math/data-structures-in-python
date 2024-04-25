d4a20d8e5ff40434aca2033460310a558af8e75c
Srujana-Math <167081923+Srujana-Math@users.noreply.github.com>
2024-04-25 16:33:29 +0530

    right view of a binary tree

---
+++ treeview2.py
@@ -0,0 +1,46 @@
+class TreeNode:
+    def __init__(self, data):
+        self.data = data 
+        self.left = None 
+        self.right = None
+      
+  def rightViewOfBinaryTree(root):
+    result = []
+    Q = [root]
+ 
+    while len(Q) > 0:
+        n = len(Q)
+        for i in range(n):
+
+            curr = Q.pop(0)
+            if i == n - 1:
+                result.append(curr.data)
+
+            if curr.left != None:
+                Q.append(curr.left)
+            if curr.right != None:
+                Q.append(curr.right)
+ 
+    print("Right view is:")
+    print(result)
+
+root = TreeNode(18)
+
+obj2 = TreeNode(15)
+obj3 = TreeNode(20)
+obj4 = TreeNode(25)
+obj5 = TreeNode(30)
+obj6 = TreeNode(45)
+obj7 = TreeNode(80)
+
+root.left = obj2 
+root.right = obj3 
+ 
+obj2.left = obj4 
+obj2.right = obj5 
+ 
+obj3.left = obj6 
+obj3.right = obj7
+
+rightViewOfBinaryTree(root)
+ 
