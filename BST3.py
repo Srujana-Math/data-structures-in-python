e91acee2c4571b7c563c7d2009041a57330e53f3
Srujana-Math <167081923+Srujana-Math@users.noreply.github.com>
2024-04-26 16:37:16 +0530

    finding the second smallest element in the binary search tree

---
+++ BST3.py
@@ -0,0 +1,37 @@
+n=int(input())
+num=list(map(int,input().split()))
+k=int(input())
+class TreeNode:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+        
+def insertIntoBST(root,ele):
+    newblock=TreeNode(ele)
+    if root== None:
+        return newblock
+
+    if root.data<ele:
+        root.right=insertIntoBST(root.right,ele)
+    else:
+        root.left=insertIntoBST(root.left,ele)
+    return root
+
+def fillInOrder(inorder,root):
+    if root ==None:
+        return
+
+    fillInOrder(inorder,root.left)
+    inorder.append(root.data)
+    fillInOrder(inorder,root.right)
+
+
+
+root=None
+for ele in num:
+    root=insertIntoBST(root,ele)
+    
+inorder=[]
+fillInOrder(inorder,root)
+print(inorder[k-1])
